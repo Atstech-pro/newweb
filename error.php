abcaaa4ea7a456520da6d1f18000d480df66a4c8
Atstech-pro <Atstech91gmail.com>
2020-04-13 00:00:05 +0300

    added files

--- error.php
+++ error.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Ошибка 404</title>
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/main.css">
-	<link rel="icon" href="img/favicon.ico">
+  <?php
+    $website_title = 'Ошибка 404';
+    require 'blocks/head.php'
+  ?>
 </head>
 <body>
-  <?php require 'blocks/header.php' ?>
-  Ошибка 404, перейдите на <a href="index.php">главную</a> страницу!
+  <?php
+    require 'blocks/header.php'
+  ?>
+  <h3>Ошибка 404, перейдите на <a href="index.php">главную</a> страницу!</h3>
+
   <?php require 'blocks/footer.php' ?>
 </body>
 </html>
--- reg.php
+++ reg.php
@@ -8,11 +8,27 @@
 </head>
 <body>
 	<?php require 'blocks/header.php'; ?>
-
 	<main class="container mt-5">
 		<div class="row">
 			<div class="col-md-8">
-				Registration
+				<h4>Форма регистрации</h4>
+				<form action="reg/reg.php" method="post">
+					<label for="username">Ваше имя</label>
+					<input type="text" name="username" autocomplete="off" id="username" class="form-control mb-3">
+
+					<label for="email">Ваш email</label>
+					<input type="email" name="email" autocomplete="off" id="email" class="form-control mb-3">
+
+					<label for="login">Логин</label>
+					<input type="text" name="login" autocomplete="off" id="login" class="form-control mb-3">
+
+					<label for="pass">Пароль</label>
+					<input type="password" name="pass" autocomplete="off" id="pass" class="form-control mb-3">
+
+					<button type="submit" class ="btn btn-success mt-5">
+						Зарегестрироваться
+					</button>
+				</form>
 			</div>
 
 			<?php require 'blocks/aside.php'; ?>
